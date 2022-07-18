0bd5e92b32cb2af18ea65b68db2b4d091546d444
Geovannalc <linsgeovanna@outlook.com>
2022-07-18 15:08:24 -0300

    termino da aula devagram

---
+++ pages/api/feed.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import {validarTokenJWT} from '../../middlewares/validarTokenJWT'
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import { PublicacaoModel } from "../../models/PublicacaoModel";
+
+const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
+    try{
+        if(req.method === 'GET'){
+            if(req?.query?.id){
+                //agora q tenho o id do usuario
+                // como eu valido se o usuario e valido
+                const usuario = await UsuarioModel.findById(req?.query?.id);
+                if(!usuario){
+                    return res.status(400).json({erro : 'Usuario não encontrado'})
+                }
+
+                // E COMO EU BUSCO AS PUBLICAÇÕES DELE?
+                const publicacoes = await PublicacaoModel
+                .find({idUsuario : usuario._id})
+                .sort({data : -1});
+
+                return res.status(200).json({publicacoes});
+        }
+
+
+        }
+        return res.status(405).json({erro : 'Metodo informado não é valido'})
+
+    }catch(e){
+        console.log(e);
+    }
+    return res.status(400).json({erro : 'Não foi possivel obter o feed '})
+}
+
+export default validarTokenJWT(conectarMongoDB(feedEndpoint));
